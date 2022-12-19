06a873dab3699c0b81cf516977ce078cad9e10f2
moraruvalentyn <moraruvalentyn@gmail.com>
2022-12-19 11:19:36 +0200

    Create module

---
+++ registration.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Author: Valentyn Moraru
+ * Wishlist ajax module
+ */
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'V-Moraru_Wishlist', __DIR__);
